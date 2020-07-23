b86784a368c50d0b89d8e244d42db84b03111378
Muhammad Masood <muhammadmasood673@gmail.com>
2020-07-23 12:15:11 +0500

    Updated Animation
    useWebAnimations and getAnimation aren't working

--- src/Animation/Animation.js
+++ src/Animation/Animation.js
@@ -1,41 +1,40 @@
-import React, {  } from 'react';
+import React from 'react';
 import Car from '../Images/Car.gif';
 import Sun from '../Images/Sun.gif';
 import Ship from '../Images/Ship.gif';
 import './Animation.css';
-import useWebAnimations from "@wellyshen/use-web-animations";
+// import useWebAnimations from "@wellyshen/use-web-animations";
 
 
 function App() {
 
-  const {ref,getAnimation} = useWebAnimations({
-    keyframes:[{
-      transfrom: ["translate(1000px,0px)"]
-    }],
-    timing:{
-      duration:1000,
-      iterations:Infinity,
-    },
-  });
-     
-    const Play = () => {
-      getAnimation().play();
-    }
+  // const { ref:figure } = useWebAnimations({
+  //   keyframes: {
+  //     transform: ['translateX(600px)']
+  //   },
+  //   timing: {
+  //     duration: 3000,
+  //     iterations: Infinity
+  //   }
+  // });
+
 
   return (
     <div>
-      <button type="button" className="btn btn-primary mt-2" onClick={Play}>Play</button>
+      <button type="button" className="btn btn-primary mt-2" >Play</button>
       <button type="button" className="btn btn-secondary mt-2">Pause</button>
       <button type="button" className="btn btn-warning mt-2">Speed Up</button>
       <button type="button" className="btn btn-danger mt-2">Jump</button>
       <div className="sun-div">
-        <img src={Sun} alt="Sun" width={200} height={200} />
+        <img src={Sun} alt="Sun" width={250} height={250} />
       </div>
-      <div className="ship-div">
-        <img src={Ship} alt="Ship" width={200} height={200} />
-      </div>
-      <div className="car-div" ref={ref}>
-        <img src={Car} alt="Car" width={350} height={250} />
+      <div className="" >
+        <div className="ship-div">
+          <img src={Ship} alt="Ship" width={250} height={250} />
+        </div>
+        <div className="car-div">
+          <img src={Car} alt="Car" width={400} height={250} />
+        </div>
       </div>
     </div>
   );
